0813b0a8a1446655b5e1a2c7b551b55aee3a2252
BESLINML <beslin221101@gmail.com>
2026-08-27 15:40:03 +0530

    Fix AuthApi import case

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { loginUser } from "../api/AuthApi";
+import { loginUser } from "../Api/AuthApi";
 
 
 export default function Login() {

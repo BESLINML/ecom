46c15a450c0937bf015742322fa44babb67065ce
BESLINML <beslin221101@gmail.com>
2026-08-28 10:16:50 +0530

    Fix CORS for Netlify

--- demo/demo/src/main/java/com/example/demo/WebConfig.java
+++ demo/demo/src/main/java/com/example/demo/WebConfig.java
@@ -12,7 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("https://dynamic-jelly-ad6cf3.netlify.app")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://dynamic-jelly-ad6cf3.netlify.app"
+                )
                 .allowedMethods(
                         "GET",
                         "POST",

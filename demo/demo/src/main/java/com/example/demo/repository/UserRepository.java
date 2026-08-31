f94953587b5fbcb39595641d308be87e673c6849
BESLINML <beslin221101@gmail.com>
2026-08-31 12:41:25 +0530

    Fix user login database lookup

--- demo/demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ demo/demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,4 +12,8 @@ public interface UserRepository
 
     Optional<User> findByEmail(String email);
 
+    @Query(value = "SELECT * FROM users WHERE email = :email", nativeQuery = true)
+    Optional<User> findUserByEmailNative(
+            @Param("email") String email
+    );
 }
